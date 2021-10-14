07690bf65aa87929ef3fd82c973a4142adddf657
Lionatic <ahmadhakim404@gmail.com>
2021-10-14 12:07:01 +0700

    create article controller for the rest

---
+++ app/controllers/article.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Artcile = db.article;
+
+// Create and Save a new Article
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Articles from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Article with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Article by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Article with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Articles from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published Articles
+exports.findAllPublished = (req, res) => {
+  
+};
